183cafe53b19f37ca44a299e9d80740a24d7f319
Boar <hebingchang1@live.com>
2017-11-15 14:20:52 +0800

    Try to add Jaccount(oauth2) Provider

---
+++ src/Provider/Jaccount.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Entity\User;
+use League\OAuth2\Client\Token\AccessToken as AccessToken;
+
+class Jaccount extends AbstractProvider
+{
+    public $scopes = array(
+        ''
+    );
+
+    public $responseType = 'json';
+    public function urlAuthorize()
+    {
+        return 'https://jaccount.sjtu.edu.cn/oauth2/authorize';
+    }
+
+    public function urlAccessToken()
+    {
+        return 'https://jaccount.sjtu.edu.cn/oauth2/token';
+    }
+
+    public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return 'https://api.sjtu.edu.cn/v1/me/profile?' . http_build_query([
+                'access_token' => $token->accessToken
+            ]);
+    }
+
+    public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        $response = (array) $response;
+        $school_name = '上海交通大学';
+
+        $uid = $response->account;
+        $name = $response->name;
+        $student_number = $response->code;
+
+        $user = new User;
+        $user->exchangeArray(array(
+            'uid' => $uid,
+            'name' => $name,
+            'school_name' => $school_name,
+            'student_number' => $student_number
+        ));
+        return $user;
+    }
+
+    public function getUserUid(AccessToken $token)
+    {
+        static $response = null;
+
+        if ($response == null) {
+            $client = $this->getHttpClient();
+            $client->setBaseUrl('https://api.sjtu.edu.cn/v1/me/profile?access_token=' . $token);
+            $request = $client->get()->send();
+            $response = json_decode($request->getBody());
+        }
+
+        return $this->userUid($response, $token);
+    }
+
+    public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        $token->uid = $response->account;
+        return $response->account;
+    }
+
+    public function userEmail($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return $response->account . "@sjtu.edu.cn";
+    }
+
+    public function userScreenName($response, \League\OAuth2\Client\Token\AccessToken $token)
+    {
+        return $response->name;
+    }
+}
